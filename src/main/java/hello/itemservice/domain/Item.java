b17b083ce65082586503405fe43db6080757b2e1
MINNIT <dr94406@naver.com>
2023-09-09 23:45:37 +0900

    [feat] JPA 적용 및 CRUD 메소드 작성

--- src/main/java/hello/itemservice/domain/Item.java
+++ src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,17 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class Item {
 
+    /**  DB에서 값을 넣어주는 방식 */
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name ="item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
